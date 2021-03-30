fdd0a7204139cb704b970ecb9905991d1a3be277
danielwuhoo <danielwuhoo@gmail.com>
2021-03-29 22:28:47 -0400

    add cvs vaccine cron

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const dotenv = require('dotenv');
+const cron = require('node-cron');
+const fetch = require("node-fetch");
 
 const client = new Discord.Client();
 client.commands = new Map();
@@ -17,6 +19,47 @@ client.config = {
 	google: process.env.GOOGLE_API_KEY,
 	echo: process.env.ECHO
 }
+
+const cities = [
+	'ALEXANDRIA',
+	'ANNANDALE',
+	'ARLINGTON',
+	'BAILEYS CROSSROADS',
+	'BURKE',
+	'CHANTILLY',
+	'FAIRFAX',
+	'FALLS CHURCH',
+	'GAINESVILLE',
+	'HERNDON',
+	'MANASSAS',
+	'OAKTON',
+	'RESTON',
+	'ROSSLYN',
+	'SPRINGFIELD',
+	'STERLING',
+	'VIENNA',
+	'WOODBRIDGE'
+];
+
+const grabData = async () => {
+	const response = await fetch("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.VA.json?vaccineinfo", {
+		"headers": {
+		  "referer": "https://www.cvs.com/immunizations/covid-19-vaccine",
+		},
+		"method": "GET"
+	  });
+	const data = (await response.json()).responsePayloadData.data.VA;
+	const available = data.filter(e => cities.includes(e.city) && e.status != 'Fully Booked');
+
+	if (available.length) {
+		const mainChannel = await client.channels.fetch('141703897659080704');
+		mainChannel.send(`<@&209490258859917313> Vaccines available in ${available.map(e => e.city).join()} \n https://www.cvs.com/immunizations/covid-19-vaccine`);
+	}
+}
+
+cron.schedule('* * * * *', () => {
+	grabData();
+});
 
 fs.readdir("./commands/", (err, files) => {
 	if (err) {

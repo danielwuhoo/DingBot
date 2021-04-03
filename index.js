edf261734f5f3efec38100f4e0b062ad12119a54
danielwuhoo <danielwuhoo@gmail.com>
2021-04-03 16:33:11 -0400

    Revert "add cvs vaccine cron"
    This reverts commit fdd0a7204139cb704b970ecb9905991d1a3be277.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const dotenv = require('dotenv');
-const cron = require('node-cron');
-const fetch = require("node-fetch");
 
 const client = new Discord.Client();
 client.commands = new Map();
@@ -19,73 +17,6 @@ client.config = {
 	google: process.env.GOOGLE_API_KEY,
 	echo: process.env.ECHO
 }
-
-const cities = [
-	'ALEXANDRIA',
-	'ANNANDALE',
-	'ARLINGTON',
-	'BAILEYS CROSSROADS',
-	'BURKE',
-	'CHANTILLY',
-	'FAIRFAX',
-	'FALLS CHURCH',
-	'GAINESVILLE',
-	'HERNDON',
-	'MANASSAS',
-	'OAKTON',
-	'RESTON',
-	'ROSSLYN',
-	'SPRINGFIELD',
-	'STERLING',
-	'VIENNA',
-	'WOODBRIDGE'
-];
-
-const grabData = async () => {
-	const response = await fetch("https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.VA.json?vaccineinfo", {
-		"headers": {
-		  "referer": "https://www.cvs.com/immunizations/covid-19-vaccine",
-		},
-		"method": "GET"
-	  });
-	const data = (await response.json()).responsePayloadData.data.VA;
-	const available = data.filter(e => cities.includes(e.city) && e.status != 'Fully Booked');
-
-	if (available.length) {
-		const mainChannel = await client.channels.fetch('782426150931529791');
-		mainChannel.send(`<@&209490258859917313> Vaccines available in ${available.map(e => e.city).join()} \n https://www.cvs.com/immunizations/covid-19-vaccine`);
-	}
-}
-
-const grabOtherData = async () => {
-	const response = await fetch("https://www.cvs.com/Services/ICEAGPV1/immunization/1.0.0/getIMZStores", {
-		"headers": {
-		  "accept": "application/json",
-		  "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-		  "content-type": "application/json",
-		  "sec-ch-ua-mobile": "?1",
-		  "sec-fetch-dest": "empty",
-		  "sec-fetch-mode": "cors",
-		  "sec-fetch-site": "same-origin",
-		},
-		"referrer": "https://www.cvs.com/vaccine/intake/store/cvd-store-select/first-dose-select",
-		"referrerPolicy": "strict-origin-when-cross-origin",
-		"body": "{\"requestMetaData\":{\"appName\":\"CVS_WEB\",\"lineOfBusiness\":\"RETAIL\",\"channelName\":\"MOBILE\",\"deviceType\":\"AND_MOBILE\",\"deviceToken\":\"7777\",\"apiKey\":\"a2ff75c6-2da7-4299-929d-d670d827ab4a\",\"source\":\"ICE_WEB\",\"securityType\":\"apiKey\",\"responseFormat\":\"JSON\",\"type\":\"cn-dep\"},\"requestPayloadData\":{\"selectedImmunization\":[\"CVD\"],\"distanceInMiles\":35,\"imzData\":[{\"imzType\":\"CVD\",\"ndc\":[\"59267100002\",\"59267100003\",\"59676058015\",\"80777027399\"],\"allocationType\":\"1\"}],\"searchCriteria\":{\"addressLine\":\"22032\"}}}",
-		"method": "POST",
-		"mode": "cors"
-	  });
-
-	const data = await response.json();
-	if (data.responseMetaData.statusCode == '0000') {
-		const mainChannel = await client.channels.fetch('782426150931529791');
-		mainChannel.send(`<@&209490258859917313> Vaccines available in ${data.responsePayloadData.locations.map(e => e.addressLine).join()} \n https://www.cvs.com/immunizations/covid-19-vaccine`);
-	}
-}
-
-cron.schedule('*/5 * * * *', () => {
-	grabData();
-	grabOtherData();
-});
 
 fs.readdir("./commands/", (err, files) => {
 	if (err) {
